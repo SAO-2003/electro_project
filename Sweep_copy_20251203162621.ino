335660f2ec10d0f181d42a3af29a9073d3099682
SAO-2003 <saidiaghiles2003@gmail.com>
2025-12-03 16:37:41 +0100

    second commit

--- Sweep_copy_20251203162621.ino
+++ Sweep_copy_20251203162621.ino
@@ -15,17 +15,4 @@ void loop() {
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
-
-  // Read echo time
-  long duration = pulseIn(ECHO_PIN, HIGH);
-
-  // Calculate distance in cm
-  float distance = duration * 0.0343 / 2;
-
-  // Print actual value
-  Serial.print("Distance: ");
-  Serial.print(distance, 2); // show 2 decimal places
-  Serial.println(" cm");
-
-  delay(500); // wait 0.5 seconds before next reading
-}
+  
